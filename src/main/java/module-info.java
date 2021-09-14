94c00f32471aa25cb1f0953a43c06e99c2130e73
ChrisMethsillo <christian.parra@ug.uchile.cl>
2021-09-14 08:07:46 -0300

    module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,12 @@ module com.example.aventurasdemarcoyluis {
     requires javafx.fxml;
 
 
-    opens com.example.aventurasdemarcoyluis to javafx.fxml;
-    exports com.example.aventurasdemarcoyluis;
+    exports com.example.aventurasdemarcoyluis.model.npc;
+    opens com.example.aventurasdemarcoyluis.model.npc to javafx.fxml;
+    exports com.example.aventurasdemarcoyluis.model.playablechar;
+    opens com.example.aventurasdemarcoyluis.model.playablechar to javafx.fxml;
+    exports com.example.aventurasdemarcoyluis.model.items;
+    opens com.example.aventurasdemarcoyluis.model.items to javafx.fxml;
+    exports com.example.aventurasdemarcoyluis.model;
+    opens com.example.aventurasdemarcoyluis.model to javafx.fxml;
 }
